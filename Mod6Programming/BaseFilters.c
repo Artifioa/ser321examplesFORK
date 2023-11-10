696290bcdff2d7e41cefd1408dd708c6ed8d7f14
Artifioa <artificialanomaly@gmail.com>
2023-11-09 18:48:50 -0700

    c

--- Mod6Programming/BaseFilters.c
+++ Mod6Programming/BaseFilters.c
@@ -93,7 +93,24 @@ void draw_holes(unsigned char* input_pixels, unsigned char* output_pixels, int i
 void swiss_cheese(unsigned char* input_pixels, unsigned char* output_pixels, int image_width, int image_height) {
 	// Define the average radius of the holes
 	int average_radius = round(0.08 * fmin(image_width, image_height));
-
+	draw_holes(input_pixels, output_pixels, image_width, image_height, average_radius);
+	// Loop through each pixel in the image
+	for (int y = 0; y < image_height; y++) {
+		for (int x = 0; x < image_width; x++) {
+			// Calculate the index of the current pixel in the input pixel array
+			int input_index = (y * image_width + x) * 3;
+			// Apply the yellow tint to the pixel
+			unsigned char* tinted_pixel = yellow_tint(&input_pixels[input_index]);
+			// Calculate the index of the current pixel in the output pixel array
+			int output_index = (y * image_width + x) * 3;
+			// Store the tinted pixel in the output pixel array
+			output_pixels[output_index] = tinted_pixel[0];
+			output_pixels[output_index + 1] = tinted_pixel[1];
+			output_pixels[output_index + 2] = tinted_pixel[2];
+			// Free the memory allocated for the tinted pixel
+			free(tinted_pixel);
+		}
+	}
 	// Draw the holes in the output image
 
 }
@@ -288,7 +305,7 @@ int main(int argc, char* argv[]) {
 	fclose(input_file);
 
 	// Apply the selected filter to the image data using pthreads
-	if (filter_type == 'b') {
+	if (filter_type == 'c') {
 		// Divide image into pixel columns
 		int column_width = image_width / THREAD_COUNT;
 		int column_remainder = image_width % THREAD_COUNT;
@@ -310,7 +327,7 @@ int main(int argc, char* argv[]) {
 		for (int i = 0; i < THREAD_COUNT; i++) {
 			pthread_join(threads[i], NULL);
 		}
-	} else if (filter_type == 'c') {
+	} else if (filter_type == 'b') {
 		// Divide image into pixel columns
 		int column_width = image_width / THREAD_COUNT;
 		int column_remainder = image_width % THREAD_COUNT;
@@ -325,13 +342,17 @@ int main(int argc, char* argv[]) {
 		for (int i = 0; i < THREAD_COUNT; i++) {
 			int* arg = (int*)malloc(sizeof(int));
 			*arg = i;
-			pthread_create(&threads[i], NULL, blur_filter, arg);
+			pthread_create(&threads[i], NULL, swiss_cheese, arg);
 		}
 
 		// Wait for threads to finish
 		for (int i = 0; i < THREAD_COUNT; i++) {
 			pthread_join(threads[i], NULL);
 		}
+	}
+	else {
+		printf("Error: invalid filter type specified\n");
+		return 1;
 	}
 
 	// Write modified pixel data to output file
