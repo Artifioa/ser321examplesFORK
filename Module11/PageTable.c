d53bcf459c4201a947cb9c4ef5902a1476906217
Artifioa <artificialanomaly@gmail.com>
2023-11-27 22:24:42 -0700

    Update PageTable.c

--- Module11/PageTable.c
+++ Module11/PageTable.c
@@ -21,112 +21,11 @@ struct page_table {
 
 };
 
-struct page_table* page_table_create(int page_count, int frame_count, enum replacement_algorithm algorithm, int verbose) {
-    // Allocate memory for the page table
-    struct page_table* pt = malloc(sizeof(struct page_table));
-    pt->entries = malloc(sizeof(struct page_table_entry) * page_count);
-    pt->page_count = page_count;
-    pt->frame_count = frame_count;
-    pt->algorithm = algorithm;
-    pt->verbose = verbose;
-    pt->page_faults = 0;
-    
 
-    // Initialize the entries
-    for (int i = 0; i < page_count; i++) {
-        pt->entries[i].frame_number = -1; // Indicate that the frame is free
-        pt->entries[i].data = 0; // Clear the valid and dirty bits
-        pt->entries[i].access_count = 0; // Reset the access count
-    }
 
-    return pt;
-}
 
-void page_table_destroy(struct page_table** pt) {
-    free((*pt)->entries);
-    free(*pt);
-    *pt = NULL;
-}
 
-void page_table_access_page(struct page_table *pt, int page) {
-    // Check if the page is valid
-    if (pt->entries[page].data & 1) {
-        // The page is valid, so just increase the access count
-        pt->entries[page].access_count++;
 
-        // Update the LRU list
-        list_remove(pt->lru_list, page);
-        list_push_back(pt->lru_list, page);
-
-        // Update the MFU priority queue
-        priority_queue_increase(pt->mfu_queue, page);
-    } else {
-        // The page is not valid, so we have a page fault
-        pt->page_faults++;
-
-        // Find the first free frame
-        int free_frame = -1;
-        for (int i = 0; i < pt->page_count; i++) {
-            if (!(pt->entries[i].data & 1)) {
-                free_frame = i;
-                break;
-            }
-        }
-
-        if (free_frame != -1) {
-            // Found a free frame
-            pt->entries[free_frame].frame_number = page;
-            pt->entries[free_frame].data |= 1; // Set the valid bit
-            pt->entries[free_frame].access_count = 1; // Reset the access count
-        } else {
-            // There are no free frames
-            int replace_page;
-            switch (pt->algorithm) {
-                case FIFO:
-                    replace_page = queue_pop(pt->fifo_queue);
-                    break;
-                case LRU:
-                    replace_page = list_pop_front(pt->lru_list);
-                    break;
-                case MFU:
-                    replace_page = priority_queue_pop(pt->mfu_queue);
-                    break;
-            }
-
-            // Invalidate the old page
-            for (int i = 0; i < pt->page_count; i++) {
-                if (pt->entries[i].frame_number == replace_page) {
-                    pt->entries[i].data &= ~1; // Clear the valid bit
-                    break;
-                }
-            }
-
-            // Replace the frame
-            pt->entries[replace_page].frame_number = page;
-            pt->entries[replace_page].data |= 1; // Set the valid bit
-            pt->entries[replace_page].access_count = 1; // Reset the access count
-        }
-
-        // Add the page to the data structures for each algorithm
-        queue_push(pt->fifo_queue, page);
-        list_push_back(pt->lru_list, page);
-        priority_queue_push(pt->mfu_queue, page, pt->entries[page].access_count);
-    }
-}
-
-void page_table_display(struct page_table* pt) {
-    printf("==== Page Table ====\n");
-    printf("Mode : %s\n", pt->algorithm == FIFO ? "FIFO" : pt->algorithm == LRU ? "LRU" : "MFU");
-    printf("Page Faults : %d\n", pt->page_faults);
-    page_table_display_contents(pt);
-}
-
-void page_table_display_contents(struct page_table *pt) {
-    printf("page frame | dirty valid\n");
-    for (int i = 0; i < pt->page_count; i++) {
-        printf("   %d     %d |     %d     %d\n", i, pt->entries[i].frame_number, (pt->entries[i].data >> 1) & 1, pt->entries[i].data & 1);
-    }
-}
 
 
 
@@ -289,3 +188,142 @@ int priority_queue_pop(struct priority_queue* pq) {
     pq->size--;
     return item;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct page_table* page_table_create(int page_count, int frame_count, enum replacement_algorithm algorithm, int verbose) {
+    // Allocate memory for the page table
+    struct page_table* pt = malloc(sizeof(struct page_table));
+    pt->entries = malloc(sizeof(struct page_table_entry) * page_count);
+    pt->page_count = page_count;
+    pt->frame_count = frame_count;
+    pt->algorithm = algorithm;
+    pt->verbose = verbose;
+    pt->page_faults = 0;
+    
+
+    // Initialize the entries
+    for (int i = 0; i < page_count; i++) {
+        pt->entries[i].frame_number = -1; // Indicate that the frame is free
+        pt->entries[i].data = 0; // Clear the valid and dirty bits
+        pt->entries[i].access_count = 0; // Reset the access count
+    }
+
+    return pt;
+}
+
+void page_table_destroy(struct page_table** pt) {
+    free((*pt)->entries);
+    free(*pt);
+    *pt = NULL;
+}
+
+void page_table_access_page(struct page_table *pt, int page) {
+    // Check if the page is valid
+    if (pt->entries[page].data & 1) {
+        // The page is valid, so just increase the access count
+        pt->entries[page].access_count++;
+
+        // Update the LRU list
+        list_remove(pt->lru_list, page);
+        list_push_back(pt->lru_list, page);
+
+        // Update the MFU priority queue
+        priority_queue_increase(pt->mfu_queue, page);
+    } else {
+        // The page is not valid, so we have a page fault
+        pt->page_faults++;
+
+        // Find the first free frame
+        int free_frame = -1;
+        for (int i = 0; i < pt->page_count; i++) {
+            if (!(pt->entries[i].data & 1)) {
+                free_frame = i;
+                break;
+            }
+        }
+
+        if (free_frame != -1) {
+            // Found a free frame
+            pt->entries[free_frame].frame_number = page;
+            pt->entries[free_frame].data |= 1; // Set the valid bit
+            pt->entries[free_frame].access_count = 1; // Reset the access count
+        } else {
+            // There are no free frames
+            int replace_page;
+            switch (pt->algorithm) {
+                case FIFO:
+                    replace_page = queue_pop(pt->fifo_queue);
+                    break;
+                case LRU:
+                    replace_page = list_pop_front(pt->lru_list);
+                    break;
+                case MFU:
+                    replace_page = priority_queue_pop(pt->mfu_queue);
+                    break;
+            }
+
+            // Invalidate the old page
+            for (int i = 0; i < pt->page_count; i++) {
+                if (pt->entries[i].frame_number == replace_page) {
+                    pt->entries[i].data &= ~1; // Clear the valid bit
+                    break;
+                }
+            }
+
+            // Replace the frame
+            pt->entries[replace_page].frame_number = page;
+            pt->entries[replace_page].data |= 1; // Set the valid bit
+            pt->entries[replace_page].access_count = 1; // Reset the access count
+        }
+
+        // Add the page to the data structures for each algorithm
+        queue_push(pt->fifo_queue, page);
+        list_push_back(pt->lru_list, page);
+        priority_queue_push(pt->mfu_queue, page, pt->entries[page].access_count);
+    }
+}
+
+void page_table_display(struct page_table* pt) {
+    printf("==== Page Table ====\n");
+    printf("Mode : %s\n", pt->algorithm == FIFO ? "FIFO" : pt->algorithm == LRU ? "LRU" : "MFU");
+    printf("Page Faults : %d\n", pt->page_faults);
+    page_table_display_contents(pt);
+}
+
+void page_table_display_contents(struct page_table *pt) {
+    printf("page frame | dirty valid\n");
+    for (int i = 0; i < pt->page_count; i++) {
+        printf("   %d     %d |     %d     %d\n", i, pt->entries[i].frame_number, (pt->entries[i].data >> 1) & 1, pt->entries[i].data & 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
