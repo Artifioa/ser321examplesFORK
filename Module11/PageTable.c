708a572fe926874d42275cfd4648dd0da4790f51
Artifioa <artificialanomaly@gmail.com>
2023-11-27 20:30:54 -0700

    Update PageTable.c

--- Module11/PageTable.c
+++ Module11/PageTable.c
@@ -37,17 +37,13 @@ void page_table_destroy(struct page_table** pt) {
 }
 
 void page_table_access_page(struct page_table *pt, int page) {
-    // Check if the page is valid
     if (pt->entries[page].data & 1) {
-        // The page is valid, so just increase the access count
         pt->entries[page].access_count++;
     } else {
-        // The page is not valid, so we have a page fault
         pt->page_faults++;
 
-        // Find the first free frame
         int free_frame = -1;
-        for (int i = 0; i < pt->frame_count; i++) {
+        for (int i = 0; i < pt->page_count; i++) {
             if (!(pt->entries[i].data & 1)) {
                 free_frame = i;
                 break;
@@ -55,28 +51,24 @@ void page_table_access_page(struct page_table *pt, int page) {
         }
 
         if (free_frame != -1) {
-            // Found a free frame
             pt->entries[free_frame].frame_number = page;
-            pt->entries[free_frame].data |= 1; // Set the valid bit
+            pt->entries[free_frame].data |= 1;
+            pt->entries[free_frame].access_count = 1;
         } else {
-            // There are no free frames
             int replace_frame = 0;
             switch (pt->algorithm) {
                 case FIFO:
-                    // Replace the first frame
                     replace_frame = 0;
                     break;
                 case LRU:
-                    // Replace the least recently used frame
-                    for (int i = 1; i < pt->frame_count; i++) {
+                    for (int i = 1; i < pt->page_count; i++) {
                         if (pt->entries[i].access_count < pt->entries[replace_frame].access_count) {
                             replace_frame = i;
                         }
                     }
                     break;
                 case MFU:
-                    // Replace the most frequently used frame
-                    for (int i = 1; i < pt->frame_count; i++) {
+                    for (int i = 1; i < pt->page_count; i++) {
                         if (pt->entries[i].access_count > pt->entries[replace_frame].access_count) {
                             replace_frame = i;
                         }
@@ -84,10 +76,10 @@ void page_table_access_page(struct page_table *pt, int page) {
                     break;
             }
 
-            // Should replace the frame
+            pt->entries[replace_frame].data &= ~1;
             pt->entries[replace_frame].frame_number = page;
-            pt->entries[replace_frame].data |= 1; // Set the valid bit
-            pt->entries[replace_frame].access_count = 1; // Reset the access count
+            pt->entries[replace_frame].data |= 1;
+            pt->entries[replace_frame].access_count = 1;
         }
     }
 }
@@ -102,7 +94,7 @@ void page_table_display(struct page_table* pt) {
 void page_table_display_contents(struct page_table *pt) {
     printf("page frame | dirty valid\n");
     for (int i = 0; i < pt->page_count; i++) {
-        printf("%d %d | %d %d\n", i, pt->entries[i].frame_number, (pt->entries[i].data >> 1) & 1, pt->entries[i].data & 1);
+        printf("   %d     %d |     %d     %d\n", i, pt->entries[i].frame_number, (pt->entries[i].data >> 1) & 1, pt->entries[i].data & 1);
     }
 }
 
@@ -132,7 +124,7 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < scenario->refstr_len; i++) {
             page_table_access_page(pt, scenario->refstr[i]);
         }
-
+        
         page_table_display(pt);
         page_table_destroy(&pt);
     }
