e35c8a961acf39e5f97a39c4b1e0952baecfd63a
Artifioa <artificialanomaly@gmail.com>
2023-11-27 23:41:34 -0700

    Update Simulator.c

--- Module11/Simulator.c
+++ Module11/Simulator.c
@@ -19,18 +19,6 @@ int main(int argc, char* argv[]) {
     struct page_table* pt_mfu = page_table_create(data->page_count, data->frame_count, MFU, 0);
 
     //simulate page requests: fifo
-    for (int i = 0; i < data->refstr_len; i++) {
-        page_table_access_page(pt_fifo, data->refstr[i]);
-        //page_table_display_contents(pt_fifo);
-    }
-    page_table_display(pt_fifo);
-
-    //simulate page requests: lru
-    for (int i = 0; i < data->refstr_len; i++) {
-        page_table_access_page(pt_lru, data->refstr[i]);
-        //page_table_display_contents(pt_lru);
-    }
-    page_table_display(pt_lru);
 
     //simulate page requests: mfu
     for (int i = 0; i < data->refstr_len; i++) {
