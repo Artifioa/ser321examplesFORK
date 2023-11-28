8f47e1c8f638d21e508d17b7e68f640df1df88d0
Artifioa <artificialanomaly@gmail.com>
2023-11-27 23:06:21 -0700

    Update PageTable.c

--- Module11/PageTable.c
+++ Module11/PageTable.c
@@ -67,15 +67,15 @@ void page_table_access_page(struct page_table *pt, int page) {
         pt->page_faults++;
 
         // Find the first free frame
-        int free_frame = 0;
+        int free_frame = -1;
         for (int i = 0; i < pt->frame_count; i++) {
-            if (pt->entries[i].frame_number == 0) {
+            if (pt->entries[i].frame_number == -1) {
                 free_frame = i;
                 break;
             }
         }
 
-        if (free_frame != 0) {
+        if (free_frame != -1) {
             // Found a free frame
             pt->entries[free_frame].frame_number = page;
             pt->entries[free_frame].data |= 1; // Set the valid bit
